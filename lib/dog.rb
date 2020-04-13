7bc8b4781dfe9c9864496524b01a049cd32127ed
Zoey Chesny <z.chesny@gmail.com>
2020-04-13 22:03:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 # Your code goes here!
 class Dog
 
-  def name=(dog_name)
-    @dog_name = dog_name
+  def name=(name)
+    @name = name
   end
 
   def name
-    @dog_name
+    @name
   end
 
   def bark
